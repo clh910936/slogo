a2724f802f03c044eae23ec5caf615cae1f08d87
Carrie Hunner <clh87@duke.edu>
2019-02-21 22:07:18 -0500

    added language drop down tab and creates a properties file

--- src/Console.java
+++ src/Console.java
@@ -2,27 +2,32 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class Console {
     private Stage myStage;
     private BorderPane myBorderPane;
     private HBox myTextHBox;
     private GridPane myButtonGridPane;
+    private HBox myErrorBox;
+    private ComboBox myLanguageDropDown;
+
+    private ResourceBundle myResourcesBundle;
+    private String myLanguage;
 
     private Button myRunButton;
     private List<Button> myButtonList;
     private TextArea myUserInputField;
-
-    private Background myBackground;
-    private Border myBorder;
 
     private final Paint myBorderColor = Color.WHITE;
     private final int CONSOLE_WIDTH = 500;
@@ -31,6 +36,8 @@ public class Console {
     private final int BUTTON_INSET = 5;
     private final int BUTTON_PANE_WIDTH = BUTTON_WIDTH + 50;
     private final int BUTTON_VGAP = 10;
+    private final String RESOURCE_FILENAME = "Console";
+    private final String RUN_BUTTON = "RUN_BUTTON";
 
     private Insets myButtonInsets;
 
@@ -39,40 +46,58 @@ public class Console {
         myStage = stage;
         myButtonList = new ArrayList<>();
         myButtonGridPane = new GridPane();
-
-        //TODO: initialize Pane method
         myBorderPane = new BorderPane();
-
-        //TODO: initialize common graphics method CHECK IF CORNER RADII
-        myBackground = new Background(new BackgroundFill(myBorderColor, CornerRadii.EMPTY, Insets.EMPTY));
-        myBorder = new Border(new BorderStroke(myBorderColor, BorderStrokeStyle.SOLID, new CornerRadii(0), new BorderWidths(1)));
+        myErrorBox = new HBox();
+        myLanguageDropDown = new ComboBox();
         myButtonInsets = new Insets(BUTTON_INSET, BUTTON_INSET, BUTTON_INSET, BUTTON_INSET);
+        //myResourcesBundle = ResourceBundle.getBundle(RESOURCE_FILENAME);
 
-        //TODO: initialize user field
         myUserInputField = new TextArea();
         myTextHBox = new HBox(myUserInputField);
 
-        myRunButton = createAndFormatButton("Run");
-        myRunButton.setOnMouseClicked(e -> readText());
+        //TODO: format button grid pane
+        formatButtonGridPane();
+        createRunButton();
+        addButtons();
 
-
-        initializeButtonGridPane();
+        initializeDropDown();
 
         myBorderPane.setCenter(myTextHBox);
+        myButtonGridPane.add(myLanguageDropDown, 0, 1);
         myBorderPane.setRight(myButtonGridPane);
+
 
         Scene consoleScene = new Scene(myBorderPane, CONSOLE_WIDTH, CONSOLE_HEIGHT);
         myStage.setScene(consoleScene);
     }
 
-    private void readText() {
-        String input = myUserInputField.getText();
+    //TODO: fix magic values
+    //TODO: bind language to variable input into parse
+    private void initializeDropDown(){
+        ArrayList<String> languages = new ArrayList<>();
+        languages.add("English");
+        languages.add("French");
+        myLanguageDropDown.getItems().addAll(languages);
+        myLanguageDropDown.setValue("English");
     }
 
-    private void initializeButtonGridPane(){
+    private void formatButtonGridPane() {
         myButtonGridPane.setPrefWidth(BUTTON_PANE_WIDTH);
         myButtonGridPane.setAlignment(Pos.TOP_CENTER);
         myButtonGridPane.setVgap(BUTTON_VGAP);
+    }
+
+    private void createRunButton() {
+        myRunButton = createAndFormatButton("Run");
+        myRunButton.setOnMouseClicked(e -> readText());
+    }
+
+    private void readText() {
+        String input = myUserInputField.getText();
+        System.out.println(input);
+    }
+
+    private void addButtons(){
         for(int k = 0; k < myButtonList.size(); k++){
             myButtonGridPane.add(myButtonList.get(k), 0, k);
         }
