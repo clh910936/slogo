4d40d29418fae3ceb454b7951e53b5d7835021a2
Michael Zhang <sz114@duke.edu>
2019-02-27 23:53:51 -0500

    updating turtles

--- src/Backend/src/Commands/ArcTangent.java
+++ src/Backend/src/Commands/ArcTangent.java
@@ -1,12 +1,13 @@
 package Commands;
 
+import Models.ModelManager;
 import Models.Turtle;
 import Models.UserDefinedCommandsModel;
 import Models.VariablesModel;
 
 public class ArcTangent extends OneParamCommand {
-    public ArcTangent(String language, Turtle turtle, VariablesModel variablesModel, UserDefinedCommandsModel userDefinedCommandsModel) {
-        super(language, turtle, variablesModel, userDefinedCommandsModel);
+    public ArcTangent(String language, ModelManager modelManager) {
+        super(language, modelManager);
     }
     @Override
     public double executeCommand() throws ClassCastException {
