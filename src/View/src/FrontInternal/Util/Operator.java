9efe443edcf9ea5b937894d549fd9b71c8c9fe6b
Carrie Hunner <clh87@duke.edu>
2019-03-01 17:26:10 -0500

    created Operator clas

---
+++ src/View/src/FrontInternal/Util/Operator.java
@@ -0,0 +1,61 @@
+package FrontInternal.Util;
+
+import BackExternal.Creator;
+import BackExternal.IModelManager;
+import BackExternal.ViewAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Operator {
+    IModelManager myManager;
+    List<ViewAPI> myViews;
+    String DEFAULT_LANGAUGE = "English";
+
+    Operator(){
+        myViews = new ArrayList<>();
+        Creator creator = new Creator();
+        myManager = creator.getModelManager();
+    }
+
+    /**
+     * Adds a View that will have update called
+     * every time Operator.parse() is called
+     * @param view concrete implementation of ViewAPI
+     */
+    public void addViewToUpdate(ViewAPI view){
+        myViews.add(view);
+    }
+
+    /**
+     * Sends the given command to the parser through the ModelManager.
+     * Note: the language is set to a default such that
+     * programmers can call this and pass a command without needing to specify
+     * a language.
+     * @param command String of the command to execute.
+     */
+    public void parse(String command){
+        parse(command, DEFAULT_LANGAUGE);
+    }
+
+
+    /**
+     * Sends the given command to the parser through the ModelManager.
+     * @param command String of Command to execute
+     * @param language String of Langauge to execute
+     */
+    public void parse(String command, String language){
+        myManager.parseCommand(command, language);
+        for(ViewAPI v : myViews){
+            v.update();
+        }
+    }
+
+    /**
+     * Returns IModelManager
+     * @return instance of IModelManager associated with the current display
+     */
+    public IModelManager getManager(){
+        return myManager;
+    }
+}
--- src/View/src/FrontInternal/Views/AllUserViews.java
+++ src/View/src/FrontInternal/Views/AllUserViews.java
@@ -1,4 +1,27 @@
 package FrontInternal.Views;
 
-public class AllUserViews {
+import BackExternal.IModelManager;
+import BackExternal.ViewAPI;
+import javafx.scene.control.ComboBox;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ResourceBundle;
+
+public class  AllUserViews implements ViewAPI extends ComboBox {
+    private ResourceBundle myResources;
+    private IModelManager myManager;
+
+    AllUserViews(IModelManager manager){
+        myResources = ResourceBundle.getBundle("ViewDropDown");
+        myManager = manager;
+    }
+    private ViewAPI makeView(String s) {
+        Class c = Class.forName("FrontInternal.Views." + s);
+
+        var constructor = c.getConstructor(String.class, int.class);
+
+        return (Bet) constructor.newInstance(description, odds);
+    }
+
+
 }
