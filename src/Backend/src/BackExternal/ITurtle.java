5428a5bb213311b518c22e0a0b248c8df07cd9cb
Michael Zhang <sz114@duke.edu>
2019-03-02 18:06:55 -0500

    list trimming is working correctly

--- src/Backend/src/BackExternal/ITurtle.java
+++ src/Backend/src/BackExternal/ITurtle.java
@@ -10,12 +10,15 @@ public interface ITurtle {
     List<Double> getHeadingAngle();
     List<Boolean> getIsDisplayed();
 
+//    1  2  3  4  5  6
+//      1  0  1  0  1  0
     //background index
     //pencolor index
     //pensize pixels
     //setshape index
     //setpalette ??? talk to feroze and carrie
-    //
+
+    //boolean getReceive();
 
 //    List<Integer> getBackGroundIndex();
 //    List<Integer> getPenColorIndex();
