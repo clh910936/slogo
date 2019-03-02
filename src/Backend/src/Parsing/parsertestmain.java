d902ab003f5e00620ed544c8de57a469d86738e2
Michael Zhang <sz114@duke.edu>
2019-03-02 12:14:42 -0500

    making if

--- src/Backend/src/Parsing/parsertestmain.java
+++ src/Backend/src/Parsing/parsertestmain.java
@@ -50,35 +50,34 @@ public class parsertestmain {
 //                "    pu fd :size pd fd :size\n" +
 //                "  ]\n" +
 //                "]\ndash 20 8", "English"));
-        System.out.println(cp.parseCommand("to square [ :distance ]\n" +
-                "[\n" +
-                "  repeat 4 [\n" +
-                "    fd :distance\n" +
-                "    rt 90\n" +
-                "  ]\n" +
-                "]\n" +
-                "\n" +
-                "\n" +
-                "to face [ ]\n" +
-                "[\n" +
-                "  pendown square 100\n" +
-                "  penup forward 20\n" +
-                "  right 90 forward 25\n" +
-                "  pendown forward 50\n" +
-                "  penup back 75\n" +
-                "  left 90 forward 65\n" +
-                "  right 90 forward 20\n" +
-                "  pendown square 15\n" +
-                "  penup forward 45\n" +
-                "  pendown square 15           \n" +
-                "  penup back 15\n" +
-                "  right 90 forward 20 left 45\n" +
-                "  pendown square 20\n" +
-                "]\n" +
-                "\n" +
-                "\n" +
-                "face\n", "English"));
-        System.out.println(mm.getVariables());
+//        System.out.println(cp.parseCommand("to square [ :distance ]\n" +
+//                "[\n" +
+//                "  repeat 4 [\n" +
+//                "    fd :distance\n" +
+//                "    rt 90\n" +
+//                "  ]\n" +
+//                "]\n" +
+//                "\n" +
+//                "\n" +
+//                "to face [ ]\n" +
+//                "[\n" +
+//                "  pendown square 100\n" +
+//                "  penup forward 20\n" +
+//                "  right 90 forward 25\n" +
+//                "  pendown forward 50\n" +
+//                "  penup back 75\n" +
+//                "  left 90 forward 65\n" +
+//                "  right 90 forward 20\n" +
+//                "  pendown square 15\n" +
+//                "  penup forward 45\n" +
+//                "  pendown square 15           \n" +
+//                "  penup back 15\n" +
+//                "  right 90 forward 20 left 45\n" +
+//                "  pendown square 20\n" +
+//                "]\n" +
+//                "\n" +
+//                "\n" +
+//                "face\n", "English"));
 //        System.out.println(cp.parseCommand("make :distance 50\n" +
 //                "\n" +
 //                "fd :distance\n" +
@@ -97,7 +96,17 @@ public class parsertestmain {
 //                "  rt 90\n" +
 //                "]", "English"));
 //        System.out.println(cp.parseCommand("left -360", "English"));
-        //System.out.println(cp.parseCommand("sin 180", "English"));
+        System.out.println(cp.parseCommand("to example [ :x ]\n" +
+                "[\n" +
+                "  if greater? :x 10\n" +
+                "  [\n" +
+                "    example difference :x 10\n" +
+                "  ]\n" +
+                "  fd 50\n" +
+                "  right 10\n" +
+                "]\n" +
+                "\n" +
+                "example 100", "English"));
 //        System.out.println(cp.parseCommand("dotimes [ :t 10 ] [\n" +
 //                "      fd 1\n" +
 //                "      rt / sin :t 2\n" +
