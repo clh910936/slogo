583b8da58de6e662a4fd082b9d33b3fb2030b19f
Michael Zhang <sz114@duke.edu>
2019-03-02 21:06:07 -0500

    refactoring to make a turtle abstract class

---
+++ src/Backend/src/Models/GeneralTurtle.java
@@ -0,0 +1,13 @@
+package Models;
+
+public abstract class GeneralTurtle {
+//    public static final double STARTX = 2000;
+//    public static final double STARTY = 2000;
+
+    protected double nextPointX;
+    protected double nextPointY;
+    protected boolean isPenUp;
+    protected double headingAngle;
+    protected boolean isDisplayed;
+    protected boolean isClearScreen;
+}
