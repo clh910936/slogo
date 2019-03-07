431bc5aa0601dc936aeeb3cd9f8e426fad4347c5
Carrie Hunner <clh87@duke.edu>
2019-03-07 16:53:56 -0500

    started multiple GUI

--- src/View/src/Main/Main.java
+++ src/View/src/Main/Main.java
@@ -2,17 +2,26 @@ package Main;
 
 import FrontInternal.Components.Console;
 import javafx.application.Application;
+import javafx.scene.control.TitledPane;
 import javafx.stage.Stage;
 import FrontExternal.GUI;
 
+import java.awt.*;
+
 public class Main extends Application {
     public static final String TITLE = "SLOGO";
+    private static final int HEIGHT = 600;
+    private static final int WIDTH = 500;
 
     @Override
     public void start(Stage stage) throws Exception {
         //var model = new model();
         // should pass model into this maybe
+        stage.setWidth(WIDTH);
+        stage.setHeight(HEIGHT);
         var display = new GUI();
+
+        TitledPane tPane = new TitledPane();
 
 
         stage.setTitle(TITLE);
