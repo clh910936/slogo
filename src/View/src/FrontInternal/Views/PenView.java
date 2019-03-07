0e199eff20a03a49673ef9d7bf4442bd363f8cef
Carrie Hunner <clh87@duke.edu>
2019-03-06 23:24:57 -0500

    got penview working

--- src/View/src/FrontInternal/Views/PenView.java
+++ src/View/src/FrontInternal/Views/PenView.java
@@ -1,6 +1,7 @@
 package FrontInternal.Views;
 
 import API.IModelManager;
+import FrontInternal.Components.Console;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
@@ -9,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 
+import java.lang.reflect.Method;
 import java.util.ResourceBundle;
 
 public class PenView extends View{
@@ -16,20 +18,51 @@ public class PenView extends View{
     private final static int MAX_PEN_SIZE = 20;
     private final static int SLIDER_WIDTH = 150;
     private ResourceBundle myResources;
+    private static final int BUTTON_WIDTH = 100;
+    private Console myConsole;
+    private ResourceBundle myErrorResources;
+    private ResourceBundle myGeneralResources;
 
-    public PenView(IModelManager manager){
+    public PenView(IModelManager manager, Console console){
         super(manager);
-        System.out.println("\t made it to constructor");
+        myErrorResources = ResourceBundle.getBundle("Errors");
+        myGeneralResources = ResourceBundle.getBundle("ViewBackgroundCommands");
         myVBox = new VBox();
+        myConsole = console;
         setContents(myVBox);
         myResources = ResourceBundle.getBundle("ViewBackgroundCommands");
 
         initializeSliderLabel();
         formatSlider();
+        makeAndFormatButton(myGeneralResources.getString("upButton"), myGeneralResources.getString("upMethod"));
+        makeAndFormatButton(myGeneralResources.getString("downButton"), myGeneralResources.getString("downMethod"));
+
+        myVBox.setAlignment(Pos.CENTER);
     }
 
 
+    private void makeAndFormatButton(String name, String methodName){
+        Button b = new Button();
+        b.setText(name);
+        b.setPrefWidth(BUTTON_WIDTH);
+        b.setOnMouseClicked(e -> {
+            try {
+                System.out.println(methodName);
+                Method method = this.getClass().getDeclaredMethod(methodName);
+                method.invoke(this);
+            } catch (Exception e1) {
+                myConsole.showError(myErrorResources.getString("BUTTON_ERROR"));
+            }});
+        myVBox.getChildren().add(b);
+    }
 
+    private void setPenUp(){
+        myManager.parseCommand(myGeneralResources.getString("penUpCommand"), DEFAULT_LANGUAGE);
+    }
+
+    private void setPenDown(){
+        myManager.parseCommand(myGeneralResources.getString("penDownCommand"), DEFAULT_LANGUAGE);
+    }
     private void initializeSliderLabel() {
         Label label = new Label(myResources.getString("SliderLabel"));
         myVBox.getChildren().add(label);
