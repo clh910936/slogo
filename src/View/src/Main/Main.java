81afe31c8c9c027e1cc93d2b54270efb7192aa48
Carrie Hunner <clh87@duke.edu>
2019-03-07 19:56:37 -0500

    refactoring Main

--- src/View/src/Main/Main.java
+++ src/View/src/Main/Main.java
@@ -11,13 +11,11 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
-import java.beans.EventHandler;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Main extends Application {
-    public static final String TITLE = "SLOGO";
     private static final int HEIGHT = 600;
     private static final int WIDTH = 800;
     private TabPane myTabs;
@@ -28,20 +26,13 @@ public class Main extends Application {
     private int myTabCount;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        System.out.println("Start");
-        myResources = ResourceBundle.getBundle("Main");
-        myConsole = new Console();
-        myTabMap = new HashMap<>();
-        //var model = new model();
-        // should pass model into this maybe
-        myTabCount = 0;
-        stage.setWidth(WIDTH);
-        stage.setHeight(HEIGHT);
-        myTabs = new TabPane();
+    public void start(Stage stage) {
+        initializeVariables();
+        initializeStage(stage);
+
         makePlusTab();
-        System.out.printf("Make new tab called from start");
         makeNewTab();
+
         myTabs.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
             @Override
             public void changed(ObservableValue<? extends Tab> observableValue, Tab tab, Tab t1) {
@@ -50,18 +41,23 @@ public class Main extends Application {
                 }
             }
         });
+        stage.show();
+    }
 
-
-
+    private void initializeStage(Stage stage) {
         Scene scene = new Scene(myTabs, HEIGHT, WIDTH);
         stage.setScene(scene);
+        stage.setTitle(myResources.getString("title"));
+        stage.setWidth(WIDTH);
+        stage.setHeight(HEIGHT);
+    }
 
-
-        stage.setTitle(TITLE);
-        stage.show();
-
-
-
+    private void initializeVariables() {
+        myResources = ResourceBundle.getBundle("Main");
+        myConsole = new Console();
+        myTabMap = new HashMap<>();
+        myTabs = new TabPane();
+        myTabCount = 0;
     }
 
     public static void main(String[] args){
@@ -71,33 +67,26 @@ public class Main extends Application {
     private void makePlusTab(){
         myPlusTab = new Tab();
         myPlusTab.setText("+");
-        myPlusTab.setOnSelectionChanged(e -> handleTabChange());
     }
 
-    private void handleTabChange() {
-//        System.out.println(myTabs.getSelectionModel().getSelectedItem().getText());
-//        if(myTabs.getSelectionModel().getSelectedItem().equals(myPlusTab)){
-//
-//        }
-    }
 
     private void makeNewTab(){
-        System.out.println("********************************");
-        System.out.println("Make new Tab called");
         myTabCount++;
         GUI display = new GUI(myConsole);
         myConsole.setModelManager(display.getModelManager());
         Tab tab = new Tab();
-        //tab.setText();
-        myTabs.getSelectionModel().select(tab);
         tab.setText(myResources.getString("Tab") + myTabCount);
-
         tab.setContent(display.getPane());
         IModelManager manager = display.getModelManager();
         myTabMap.put(tab, manager);
+        formatTabs(tab);
+    }
+
+    private void formatTabs(Tab tab) {
         myTabs.getTabs().add(tab);
         myTabs.getTabs().remove(myPlusTab);
         myTabs.getTabs().add(myPlusTab);
+        myTabs.getSelectionModel().select(tab);
         myTabs.getSelectionModel().select(myTabCount-1);
     }
 
