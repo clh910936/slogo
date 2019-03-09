11d5e294a8680d02cc1527ae6b264fae7c118733
Christina Chen <cyc25@duke.edu>
2019-03-09 03:40:23 -0400

    get rid of error trace printing for debugging

--- src/View/src/FrontInternal/Components/Console.java
+++ src/View/src/FrontInternal/Components/Console.java
@@ -193,13 +193,10 @@ public class Console extends Stage {
             Method method = this.getClass().getDeclaredMethod(s);
             method.invoke(this);
         } catch (NoSuchMethodException e1) {
-            e1.printStackTrace();
             myErrorView.displayError(myErrorResourceBundle.getString("BUTTON_ERROR"));
         } catch (IllegalAccessException e1) {
-            e1.printStackTrace();
             myErrorView.displayError(myErrorResourceBundle.getString("BUTTON_ERROR"));
         } catch (InvocationTargetException e1) {
-            e1.printStackTrace();
             myErrorView.displayError(myErrorResourceBundle.getString("BUTTON_ERROR"));
         }});
             return temp;
@@ -217,11 +214,9 @@ public class Console extends Stage {
             myManager.parseCommand(input, language);
         }
         catch (IllegalCommandException e){
-            e.printStackTrace();
             showError(myErrorResourceBundle.getString("COMMAND"));
         }
         catch (IllegalParametersException e){
-            e.printStackTrace();
             showError(myErrorResourceBundle.getString("PARAMS"));
         }
     }
